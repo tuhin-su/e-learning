1845939845d4b074606cbd52a272e0e36b1df80c
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-08-01 18:30:59 +0000

    Modify Framework and add Splash scsreen

---
+++ resource/js/main.js
@@ -0,0 +1,10 @@
+class Projector{
+    constructor(){
+        this.initialize();
+    }
+    initialize(){
+       console.log("Projector Starting");
+        let width=$(selector).width();
+        let height=$(selector).height();
+    }
+}
