312d25c587bd9ef1cd787f2ed38bc3c5941cbbe4
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-09-10 23:38:44 +0530

    Final add web management

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+export $(grep -v '^#' .env | xargs)
+
+# Validate NODE_ENV variable
+if [ -z "$MODE" ]; then
+  echo "MODE variable is not set in .env file"
+  exit 1
+fi
+
+if [ "$MODE" != "production" ] && [ "$MODE" != "development" ]; then
+  echo "MODE variable is not set to 'production' or 'development' in .env file"
+  exit 1
+fi
+
+if [ "$MODE" == "production" ]; then
+  docker-compose up -d cloudflare-tunnel
+  echo "Production mode is enabled"
+elif [ "$MODE" == "development" ]; then
+  docker-compose up -d angular
+  echo "Development mode is enabled"
+fi
