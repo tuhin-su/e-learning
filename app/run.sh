722f8feabedcb1cef2ed3b6acf156c81ff761124
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-10-02 00:06:21 +0530

    Update Docker compose
    Update Docker compose

---
+++ app/run.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+# Check if the .venv directory exists, if not, create and activate the virtual environment
+if [ ! -d ".venv" ]; then
+    pip install virtualenv
+    python3 -m virtualenv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+fi
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Run your Python script if needed
+# python3 app.py
+
+# Sleep indefinitely
+sleep infinity
