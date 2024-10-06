573ac7e80c74f1692c0c11fb17c006ef5eea1333
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-10-06 20:37:54 +0530

    anguler execute point added

--- web/timt/run.sh
+++ web/timt/run.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
-if [[ -d "node_modules" ]];; then
+
+if [ ! -d "node_modules" ] && [ ! -f "./module" ]; then
     npm install
 fi
+
 ng serve --host 0.0.0.0 --port 4200 --disable-host-check
