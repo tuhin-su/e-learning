df38f89c79f865ca5182eb31ae90626a9f107fef
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-10-17 15:41:20 +0530

    set runer

--- web/timt/run.sh
+++ web/timt/run.sh
@@ -10,5 +10,4 @@ else
     echo "API_URL environment variable is not set."
 fi
 
-# ng serve --host 0.0.0.0 --port 4200 --disable-host-check
-sleep infinty
+ng serve --host 0.0.0.0 --port 4200 --disable-host-check
