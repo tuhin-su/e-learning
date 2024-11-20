70f035a65b26269d02dcbbc87efdcf8e58d86ccc
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-11-20 10:13:17 +0000

    Update android app

--- android/eLearning/app/src/main/java/tuhin/su/github/com/e_learning/CustomWebViewClient.java
+++
@@ -1,7 +0,0 @@
-package tuhin.su.github.com.e_learning;
-
-import android.webkit.GeolocationPermissions;
-
-public interface CustomWebViewClient {
-    void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback);
-}
