b733d1252e84b8bc4a5ac1cc2f837e2d4f915603
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-11-28 20:55:07 +0000

    Update login ui

--- nginx/web/timt/src/app/login/login.component.ts
+++ nginx/web/timt/src/app/login/login.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule,MatInputModule,MatFormFieldModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
