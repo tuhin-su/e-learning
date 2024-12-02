8a3786ecfcc400a8508525425f140e420538d60a
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-12-02 14:55:26 +0000

    update autoenv

--- .autoenv.zsh
+++ .autoenv.zsh
@@ -1,3 +1,4 @@
 alias compose="docker-compose -f service.yml"
-
-alias shell="docker-compose -f service.yml exec $3 sh"
+alias shell="docker-compose -f service.yml exec"
+alias logs="docker-compose -f service.yml logs -f"
+alias activate="source .venv/bin/activate"
