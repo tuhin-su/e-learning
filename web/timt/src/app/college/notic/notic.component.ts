98391f76650105f6b09bf3827f418291e5b7d0a9
Rana-su <madhumitarana554@gmail.com>
2024-12-06 18:13:11 +0530

    notice card update

--- web/timt/src/app/college/notic/notic.component.ts
+++ web/timt/src/app/college/notic/notic.component.ts
@@ -1,16 +1,48 @@
 import { Component } from '@angular/core';
 import { MatFabButton } from '@angular/material/button';
 import {MatCardImage, MatCardModule} from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
+import { NoticeCardComponent } from "../../components/notice-card/notice-card.component";
 
 
 
 @Component({
   selector: 'app-notic',
-  imports: [MatCardModule,MatFabButton],
+  imports: [MatCardModule, MatFabButton, MatDivider, NoticeCardComponent],
   templateUrl: './notic.component.html',
   styleUrl: './notic.component.scss'
 })
 export class NoticComponent {
 
+  notices:any[]= [
+    {
+      title:"title",
+      description:"description",
+      date:"4/5/4",
+      host:"host",
+    },
+
+    {
+      title:"title",
+      description:"description",
+      date:"4/5/4",
+      host:"host",
+    },
+
+    {
+      title:"title",
+      description:"description",
+      date:"4/5/4",
+      host:"host",
+    },
+
+    {
+      title:"title",
+      description:"description",
+      date:"4/5/4",
+      host:"host",
+    },
+
+  ]
 }
