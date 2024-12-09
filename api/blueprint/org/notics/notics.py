f01803dae09ae6d04845bce5c1d2a1f4c78c8836
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-12-09 18:55:52 +0000

    add notic api

--- api/blueprint/org/notics/notics.py
+++ api/blueprint/org/notics/notics.py
@@ -2,19 +2,53 @@ from flask import Blueprint, jsonify, request, current_app
 from mysql.connector import Error
 import requests as web_rq
 from modules.DataBase import DBA
+from modules.utilty import getLabel
 
 notice = Blueprint("NoticeBord", __name__)
 
 
-@notice.route('/notice', methods=['POST', 'GET'])
-def app():
+@notice.route('/notice', methods=['GET'])
+def heandel():
+    app = current_app.config["app"]
+    @app.auth.login_required
+    def info():
+        db = DBA()
+        db.connect()
+        sql = "SELECT * FROM `notification` WHERE 1 LIMIT 10;";
+        try:
+            db.cursor.execute(sql)
+            data = db.cursor.fetchall()
+            db.disconnect()
+            return jsonify(data),200
+        except Error as e:
+            db.disconnect()
+            return jsonify({"message": str(e)}), 400
+        finally:
+            db.disconnect()
+    return info()
+
+
+@notice.route('/notice', methods=['POST'])
+def heandel_post():
     app = current_app.config["app"]
     
+    if getLabel(curent_user) != 1 and getLabel(curent_user) != 2:
+        return jsonify({"message": "Unauthorized End point"}), 400
     @app.auth.login_required
-    def heandel():
-            db = DBA()
-            user_id = app.auth.current_user()['user_id']
-            if request.method == 'GET':
-                rq = web_rq.get('https://www.timt.org.in/api/protected/notice')
-                return rq.json
-    return heandel();
+    def info():
+        curent_user = app.auth.current_user()['user_id']
+        db = DBA()
+        db.connect()
+        data = request.json
+        sql = "SELECT * FROM `notification` WHERE 1 LIMIT 10;";
+        try:
+            db.cursor.execute(sql)
+            data = db.cursor.fetchall()
+            db.disconnect()
+            return jsonify(data),200
+        except Error as e:
+            db.disconnect()
+            return jsonify({"message": str(e)}), 400
+        finally:
+            db.disconnect()
+    return info()
