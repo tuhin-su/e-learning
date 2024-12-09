d9379fd4f8233ccceaeade8ef50396b43ed21ef9
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-12-09 18:48:55 +0000

    Fix bug

--- web/timt/src/app/college/notic/notic.component.ts
+++ web/timt/src/app/college/notic/notic.component.ts
@@ -1,48 +1,119 @@
-import { Component } from '@angular/core';
-import { MatFabButton } from '@angular/material/button';
-import {MatCardImage, MatCardModule} from '@angular/material/card';
-import { MatDivider } from '@angular/material/divider';
-import { MatIcon } from '@angular/material/icon';
-import { NoticeCardComponent } from "../../components/notice-card/notice-card.component";
-
-
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { NoticCardComponent } from '../../components/notic-card/notic-card.component';
+import { FunctionaltyService } from '../../services/functionalty.service';
+import { AlertService } from '../../services/alert.service';
+import { firstValueFrom, tap } from 'rxjs';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-notic',
-  imports: [MatCardModule, MatFabButton, MatDivider, NoticeCardComponent],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    NoticCardComponent,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatFormFieldModule
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './notic.component.html',
-  styleUrl: './notic.component.scss'
+  styleUrls: ['./notic.component.scss']
 })
-export class NoticComponent {
+export class NoticComponent implements OnInit {
+  switchView: boolean = false;
+  notices: any[] = [
+    { 
+      title: "Title",
+      description: "Notic",
+      date: "Notic",
+      host: "Notic",
+      user:{
+        name: "User",
+        group: "NULL",
+        image: "sds"
+      }
 
-  notices:any[]= [
-    {
-      title:"title",
-      description:"description",
-      date:"4/5/4",
-      host:"host",
     },
+    { 
+      title: "Title",
+      description: "Notic",
+      date: "Notic",
+      host: "Notic",
+      user:{
+        name: "User",
+        group: "NULL",
+        image: "sds"
+      }
 
-    {
-      title:"title",
-      description:"description",
-      date:"4/5/4",
-      host:"host",
-    },
+    }
+  ];
 
-    {
-      title:"title",
-      description:"description",
-      date:"4/5/4",
-      host:"host",
-    },
+  noticFrom: FormGroup = new FormGroup({});
+  selectedFile: File | null = null;   
 
-    {
-      title:"title",
-      description:"description",
-      date:"4/5/4",
-      host:"host",
-    },
+  constructor(
+    private fb: FormBuilder,
+    private postService: FunctionaltyService,
+    private alertService: AlertService
+  ) { }
 
-  ]
+  ngOnInit(): void {
+
+    this.noticFrom = this.fb.group({
+      title: ['', Validators.required],
+      content: [''],
+      uploadId: ['']
+    });
+
+  }
+
+  onFileSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files[0]) {
+      this.selectedFile = fileInput.files[0];
+      this.uploadFile();
+    }
+  }
+
+  onSubmit() {
+    if (this.noticFrom.valid) {
+      console.log(this.noticFrom.value);
+    }
+  }
+
+  createNotic() {
+    this.switchView = !this.switchView;
+  }
+
+  async uploadFile() {
+    if (this.selectedFile) {
+      await firstValueFrom(this.postService.postPost(this.selectedFile).pipe(
+        tap(
+          (response) => {
+            if (response.post_id) {
+              this.noticFrom?.patchValue({
+                uploadId: response.post_id
+              });
+            }
+          },
+          (error) => {
+            console.error(error);
+            this.alertService.showWarningAlert(error.error.message);
+          }
+        )
+      ));
+    }
+  }
+
+  switchViewe() {
+    this.switchView = !this.switchView;
+  }
 }
--- web/timt/src/app/components/notic-card/notic-card.component.ts
+++ web/timt/src/app/components/notic-card/notic-card.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatButton } from '@angular/material/button';
-import {MatCardImage, MatCardModule} from '@angular/material/card';
-import { MatIcon } from '@angular/material/icon';
+import { MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
 import { capitalizeUserName } from '../../utility/function';
 import { MatCardContent } from '@angular/material/card';
 
 @Component({
   selector: 'notic-card',
-  imports: [MatCardImage, MatButton, MatIcon, MatCardModule, MatCardModule,MatDividerModule, MatCardContent],
+  imports: [
+    MatButton,
+    MatCardModule,
+    MatCardModule,
+    MatDividerModule,
+    MatCardContent],
   templateUrl: './notic-card.component.html',
   styleUrl: './notic-card.component.scss'
 })
@@ -17,6 +21,7 @@ export class NoticCardComponent implements OnInit {
     title: string;
     description: string;
     date: string;
+    host: string;
     downloadLink: string | null;
     user:{
       name: string;
@@ -24,10 +29,23 @@ export class NoticCardComponent implements OnInit {
       image: string | null;
     }
   };
+  itNew: boolean = false;
   ngOnInit(): void {
     if (this.notic) {
       this.notic.user.name = capitalizeUserName(this.notic.user.name);
       this.notic.title = capitalizeUserName(this.notic.title);
+    }
+  }
+
+  calculateNew(){
+    if (this.notic?.date){
+      const date = new Date(this.notic.date);
+      const today = new Date();
+      const diffTime = today.getTime() - date.getTime();
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      if (diffDays <= 7){
+        this.itNew = true;
+      }
     }
   }
 }
