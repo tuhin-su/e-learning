b248657d8cfbd2e2e65a0fda9c5ea28f98b54e41
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-01-24 18:08:40 +0530

    Final admin set

---
+++ web/admin/run.sh
@@ -0,0 +1,14 @@
+# checl node_module file have or not
+if [ ! -d "node_modules" ]; then
+    echo "install node_modules"
+    npm install
+fi
+
+if [[ "$MODE" == "DEVELOPMENT" ]]; then
+    echo "start development server"
+    ng serve --host 0.0.0.0 --port 5173 --disable-host-check
+else
+    echo "start production server"
+    ng build
+fi
+echo "done"
