618c9647cc7509a731f630b1944e84afa10af35d
tuhin-su <tuhinsubhraghara29195@gmail.com>
2025-02-14 22:59:04 +0000

    Fix all reload problem

---
+++ web/timt/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.timt.app',
+  appName: 'timt',
+  webDir: 'dist'
+};
+
+export default config;
