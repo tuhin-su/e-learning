eda5650ee300eafc73ddbc3598b1e8d6f5fa62d3
Rana-su <madhumitarana554@gmail.com>
2025-03-15 18:44:21 +0530

    course complete

--- web/admin/src/app/services/management.service.ts
+++ web/admin/src/app/services/management.service.ts
@@ -29,4 +29,34 @@ export class ManagementService {
     return this.http.post<any>(loginUrl, payload, { headers });
   }
 
+  deleteCourse(id: any): Observable<any>{
+    const loginUrl = `${this.api}/info/course/delete`;
+    const payload = {
+      "id": id
+    }
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.storage.get('token')}`
+    };
+    return this.http.post<any>(loginUrl, payload, { headers });
+  }
+
+  editCourse(payload:any): Observable<any>{
+    const loginUrl = `${this.api}/info/course/edit`;
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.storage.get('token')}`
+    };
+    return this.http.post<any>(loginUrl, payload, { headers });
+  }
+
+  creteCourse(payload:any): Observable<any>{
+    const loginUrl = `${this.api}/info/course/new`;
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.storage.get('token')}`
+    };
+    return this.http.post<any>(loginUrl, payload, { headers });
+  }
+
 }
